31f4b1cfaa8146663a1b41b0cd0fa26d1789f7fc
FerneyMurillo28 <ferneymurillo044@gmail.com>
2023-11-28 13:17:23 -0500

    cambios index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const jsonServer = require('json-server') 
-const server = jsonServer.create() 
-const router = jsonServer.router('db.json') 
-const middlewares = jsonServer.defaults() 
-const port = 3000   
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = 3000;
 
-server.use(middlewares) 
-server.use(router)  
-server.listen(port)
+const cors = require("cors");
+server.use(cors());
+server.use((req, res, next) => {
+  console.log(`Solicitud recibida: ${req.method} ${req.url}`);
+  next();
+});
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
